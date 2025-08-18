3740fc506cb7dae9cff0eff92c7bbe913a59c4d8
AkshatGupta <work.akshatgupta1507@gmail.com>
2025-08-18 11:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.88%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,26 +1,32 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I',1}, {'V',5}, {'X',10}, {'L',50},
-            {'C',100}, {'D',500}, {'M',1000}
-        };
-        
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int curr = roman[s[i]];
-            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
-            
-            if (curr < next) {
-                result -= curr;
-            } else {
-                result += curr;
-            }
+    int value(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
         }
+        return 0;
+    }
+
+    int romanToInt(string s) {
+        int result = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            int curr = value(s[i]);
+            int next = (i+1 < n) ? value(s[i+1]) : 0;
+
+            if (curr < next) 
+                result -= curr;  // subtract if smaller before larger
+            else 
+                result += curr;
+        }
+
         return result;
     }
 };
